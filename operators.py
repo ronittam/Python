46625bb75320a9803c536fec5ae5845022dcce34
ronittam <tamrakarronit22@gmail.com>
2024-01-18 12:32:05 +0545

    py_numbers commit

--- operators.py
+++ operators.py
@@ -19,4 +19,4 @@ print(f"floor division of {num1} and {num2} is {num1//num2}")
 if(num1>num2):
     print(f"{num1} is greater then {num2}")
 else:
-    print(f"{num2} is greater than {num1})
+    print(f"{num2} is greater than {num1}")
---
+++ py_numbers.py
@@ -0,0 +1,20 @@
+#numbers in python 
+#int,float,complete
+# a : int = 3
+# b : float = 3.012
+# c : complex = complex(1,3)
+
+# print(type(a), type(b),type(c))
+ 
+#breakpoint()
+# name="ronit tamrakar"
+# breakpoint()
+#use print(name[x:y]) to print required string
+#use print(name.split[x:y]) to split
+
+name=input("enter your name")
+print(name.split(" "))
+splitted_text=name.split(" ")
+print(f"first name is {splitted_text[0]}")
+print(f"middle name is {splitted_text[1]}")
+print(f"last name is {splitted_text[2]}")
