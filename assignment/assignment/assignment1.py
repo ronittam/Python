50cdaa52f6aa503acc418025cf7cdb32700ee295
ronittam <tamrakarronit22@gmail.com>
2024-01-23 16:03:07 +0545

    assignment1.py

--- assignment/assignment/assignment1.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program to calculate the area and circumference of a circle with a given radius from terminal.
-
-radius=float(input("Enter radius of circle: "))
-area=(22/7)*radius*radius
-circumference=2*(22/7)*radius
-print(f"Area of the circle is {area}")
-print(f"Circumference of the circle is {circumference}")
